231d1d4534e75d4bee3c276e8478be86cba95d6c
Meri Khurshudyan <71793253+MeriK-om@users.noreply.github.com>
2021-02-15 00:39:22 -0500

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -7,19 +7,24 @@ from collections import Counter
 import numpy as np
 import matplotlib.pyplot as plt
 
-_N_ = 1000
+
 
 ################### Ideal Case 2 Dice ##################################
-t = [list(range(1,7)) for x in range(2)] #creates two dice
-g = list(map(sum,list(product(*t)))) #gives all possible combinations
-z = Counter(g) #counts the occurances of each combination
-h = np.array(list(z.items())) # convert the dict to an array
-h = h.astype(np.float) # just convert it to float 
-total = sum(h[:,1]) # gives the total probabilities
-h[:,1] = h[:,1]/total*_N_ # where N is the number of times rolled the dice
+def ideal(_N_):
+    t = [list(range(1,7)) for x in range(2)] #creates two dice
+    g = list(map(sum,list(product(*t)))) #gives all possible combinations
+    z = Counter(g) #counts the occurances of each combination
+    h = np.array(list(z.items())).astype(np.float) # convert the dict to an array
+    total = sum(h[:,1]) # gives the total probabilities
+    h[:,1] = h[:,1]/total*_N_ # where N is the number of times rolled the dice
+    return h
 
+
+h = ideal(1000)
 x = h[:,0]
 y = h[:,1]
 
-plt.plot(x,y)
+plt.plot(x,y,'o')
 plt.show()
+
+#################### Experimental Case 2 Dice ###########################
