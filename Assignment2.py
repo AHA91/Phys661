c8d22d4c8fecba569be7633b19d99463862e175c
Meri Khurshudyan <71793253+MeriK-om@users.noreply.github.com>
2021-02-15 14:54:57 -0500

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -7,9 +7,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-
-
-################### Ideal Case 2 Dice ##################################
+#@@@@@@@@@@@@@@@@@@@@@@@ Needed Functions @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+######################## Ideal Case 2 Dice #############################
 def ideal(_N_, k):
     t = [list(range(1,k)) for x in range(2)] #creates two dice
     g = list(map(sum,list(product(*t)))) #gives all possible combinations
@@ -19,25 +18,47 @@ def ideal(_N_, k):
     h[:,1] = (h[:,1]/total)*_N_ # where N is the number of times rolled the dice
     return h
 
-#################### Experimental Case 2 Dice ###########################
-def experimental(_N_): #where _N_ is still the number of coin tosses
+####################### Experimental Case 2 Dice #########################
+def experimental(_N_): 
     rand1 = [random.randint(1,6) for x in range(_N_)]
     rand2 = [random.randint(1,6) for x in range(_N_)]
     h = [rand1[i]+rand2[i] for i in range(_N_)]
     g = Counter(h)
     return g
 
-
+###################### Biased Experimental Case 2 Dice ###################
+def biased(_N_):
+    rand1 = [int(random.triangular(1,6,5)) for x in range(_N_)]
+    rand2 = [int(random.triangular(1,6,6)) for x in range(_N_)]
+    h = [rand1[i]+rand2[i] for i in range(_N_)]
+    g = Counter(h)
+    return g
+    
 h = ideal(1000,7)
 x = h[:,0]
 y = h[:,1]
+
+_h_ = biased(1000)
+_x_ = _h_.keys()
+_y_ = _h_.values()
+
 
 b = experimental(1000)
 m = b.keys()
 n = b.values()
 
+
+
 '''
+####################### Remove Quotes for Part I #########################
 plt.bar(m,n, color = 'pink')
+plt.plot(x,y,'o', color = 'purple')
+plt.show()
+'''
+
+'''
+###################### Remove Quotes for Part II #########################
+plt.bar(_x_,_y_, color = 'red')
 plt.plot(x,y,'o', color = 'purple')
 plt.show()
 '''
