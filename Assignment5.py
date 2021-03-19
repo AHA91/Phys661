8cd4e6db9a2acc10aa7d5195424f045d3226ab0f
Meri Khurshudyan <71793253+Hi-Tree@users.noreply.github.com>
2021-03-19 02:49:48 -0400

    Update Assignment5.py

--- Assignment5.py
+++ Assignment5.py
@@ -2,11 +2,12 @@
 #### Assignment 5
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 ####################### Part I and II ##########################
 
-def Differentiate(points, Type, h = 1, Input = False, f = 0):
+def Differentiate(points, Type, h = 1, Input = True, f = 0):
      result = []
      if Input == True:
           f = 'lambda x:' + input('Enter a function: ')
@@ -34,28 +35,33 @@ def Part3():
      f_x = lambda x: np.sin(x) - np.cos(x)
      f_p_x = np.cos(0) + np.sin(0)
 
-     h = np.linspace(1*10**(-1),1*10**(-12),250)
+     h = np.linspace(1*10**(-1),1*10**(-12),25)
 
      table = np.zeros((len(h),3))
      table[:,0] = h
      z = []
 
      for i in h:
-          n = Differentiate([0],"TPC", f = f_x, h = i)
+          n = Differentiate([0],"TPC", f = f_x, h = i, Input = False)
           z.append(n)
           
      z = np.array(z)
      table[:,1] = np.reshape(z,len(z))
      table[:,2] = abs(table[:,1] - f_p_x)
 
-     print(table)
+     table1 = pd.DataFrame(data = table, columns = ["Step Size","TPC","Error"])
+     print(table1)
      
-     plt.plot(h, table[:,2])
+     plt.plot(h, table[:,2], "o-", color = "Pink")
      plt.xlim(1*10**(-1),0)
+     plt.xlabel("Step Size")
+     plt.ylabel("Error")
      plt.show()
 
 ######################### Part I and II ###########################
-def Integral(low, up, num, Type, Input = False, f = 0):
+integralres = []
+
+def Integral(low, up, num, Type, Input = True, f = 0):
      if Input == True:
           f = 'lambda x:' + input('Enter a function: ')
           f = eval(f)
@@ -63,33 +69,62 @@ def Integral(low, up, num, Type, Input = False, f = 0):
      if Type == "LSM":
           x = (up-low)/num
           integral = 0
+          integralres.append(integral)
           while low <= up:
                integral += (f(low)*x)
+               integralres.append(integral)
                low += x
           
      if Type == "RSM":
           x = (up-low)/num
           right = low+x
           integral = 0
+          integralres.append(integral)
           while right <= up:
                integral += (f(right)*x)
+               integralres.append(integral)
                right += x
+          integralres.append(integral)   
 
      if Type == "Trapezoid":
           x_del = (up-low)/num
-          integral = f(low) + f(up) #first and last are not 2x
+          integral = f(low)*(x_del/2)
+          integralres.append(integral)
           while low <= up-x_del:
-               integral += 2*(f(low))
+               integral += 2*(f(low))*(x_del/2)
+               integralres.append(integral)
                low += x_del
-          integral = (x_del/2*integral)
-
+          integral += f(up)*(x_del/2)
+          integralres.append(integral)
      
      if Type == "MPM":
           x = (up-low)/num
           mid = low+(x/2)
           integral = 0
+          integralres.append(integral)
           while mid <= up:
                integral += (f(mid)*x)
+               integralres.append(integral)
                mid += x
-               
-     return integral 
+
+     return integral  
+
+######################### Part III #################################
+
+def Part33():
+     table = np.zeros((26,4))
+     f_x = lambda x: x**2
+     lsm = Integral(0,10,25,"LSM", Input = False, f = f_x)
+     table[:,0] = integralres
+     integralres.clear()
+     rsm = Integral(0,10,25,"RSM", Input = False, f = f_x)
+     table[:,1] = integralres
+     integralres.clear()
+     tra = Integral(0,10,25,"Trapezoid", Input = False, f = f_x)
+     table[:,2] = integralres
+     integralres.clear()
+     mpm = Integral(0,10,25,"MPM", Input = False, f = f_x)
+     table[:,3] = integralres
+     integralres.clear()
+     table1 = pd.DataFrame(data = table, columns = ["LSM","RSM","Trapezoid","MPM"])
+     return table1
