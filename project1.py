ec77b273179afc377ab28bc10d6732530f3edb05
Meri Khurshudyan <71793253+Hi-Tree@users.noreply.github.com>
2021-05-20 00:29:50 -0400

    Update project1.py

--- project1.py
+++ project1.py
@@ -1,33 +1,31 @@
+# Meri Khurshudyan 
+# Project1
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import truncnorm
 import random as r
+import statistics as s
 
-def OneDWalk(N, section):
-     displ = None
-     sqrtNDispl = []
+def OneDWalk(M, section):
+     M = M+1
      if section == 1:
-          for i in range(int(np.sqrt(N))):
-               displ = np.cumsum(np.array(r.choices([-1,1],k=N)))#cumulitive displacement
-               sqrtNDispl.append(displ[N-1])
-
+          x_ave = [s.mean([sum(r.choices([-1,1],k=N)) for i in range(int(np.sqrt(N)))]) for N in range(1,M,1)]
+          x2_ave = [np.average(np.square([sum(r.choices([-1,1],k=N)) for i in range(int(np.sqrt(N)))])) for N in range(1,M,1)]     
+          t = "Integer -1 or 1"
      if section == 2:
-          for i in range(int(np.sqrt(N))):
-               displ = np.cumsum(np.array(np.random.uniform(-1,1.00000000000002,N)))
-               sqrtNDispl.append(displ[N-1])
-               
+          x_ave = [s.mean([sum(np.random.uniform(-1,1,N)) for i in range(int(np.sqrt(N)))]) for N in range(1,M,1)]
+          x2_ave = [np.average(np.square([sum(np.random.uniform(-1,1,N)) for i in range(int(np.sqrt(N)))])) for N in range(1,M,1)]
+          t = "Float between -1 and 1"
      if section == 3:
-          for i in range(int(np.sqrt(N))):
-               displ = np.cumsum(np.array(np.random.normal(size = N)))
-               sqrtNDdispl.append(displ[N-1])
-          
-
-     x_aver = displ
-     x2_aver = np.square(displ)
-     plt.plot(x_aver,range(N), label = "X-Average", color = "Magenta")
-     plt.plot(x2_aver,range(N),label = "X^2-Average", color = "Blue")
-     plt.xlabel("N")
-     plt.ylabel("Displacement")
+          x_ave = [s.mean([sum(np.random.normal(size=N)) for i in range(int(np.sqrt(N)))]) for N in range(1,M,1)]
+          x2_ave = [np.average(np.square([sum(np.random.normal(size=N)) for i in range(int(np.sqrt(N)))])) for N in range(1,M,1)]
+          t = "Normal Distribution"
+     
+     plt.plot(range(M),range(M), label = "Theoretical <x>^2")
+     plt.plot(range(M),[0]*M,label = "Theoretical <x>")
+     plt.plot(range(len(x_ave)),x_ave,label = "Experimental <x>")
+     plt.plot(range(len(x2_ave)),x2_ave, label = "Experimental <x>^2")
+     plt.title(t)
      plt.legend()
      plt.show()
           
