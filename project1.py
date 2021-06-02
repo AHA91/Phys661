8771a27fa44c6b3134e5c87d2c1b1db713908a8b
Meri Khurshudyan <71793253+Hi-Tree@users.noreply.github.com>
2021-06-01 21:37:59 -0400

    Add files via upload

--- project1.py
+++ project1.py
@@ -1,18 +1,31 @@
-# Meri Khurshudyan 
-# Project1
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.integrate as integrate
 import sympy as sp
 import random as r
 import statistics as s
 import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
 
+
 def KramerKronig():
-     b = sp.Symbol("b")
-     df = np.array(pd.read_csv("extinctionSpectrum.txt",delimiter = "\t"))
-     n = [1+(2/np.pi)*sp.integrate((b*df[i,1])/((b**2)-df[i,0]**2),(b,0,i)) for i in range(0,1000,4)]
-     plt.plot(range(0,1000,4),n)
+     df = np.array(pd.read_csv("extinctionSpectrum.txt",delimiter = "\t",header = None))
+     f = df[:,0]/(3*10**8)
+     n_ = []
+     o_ = []
+     n = 0
+     for m in range(1000): #w not prime
+          for i in range(999):
+               if f[i] != f[m]:
+                    #print([f[m],f[i]])
+                    n = n + ((f[i]*df[i,1])/((f[i]**2)-(f[m]**2)))*(f[i+1]-f[i])
+               else:
+                    n = n + 0
+          o = (2/np.pi)*n
+          o_.append(o)
+          n = 0
+     plt.plot(f,o_)
+     plt.plot(f, df[:,1])
      plt.show()
      
 def OneDWalk(M, section):
@@ -69,7 +82,7 @@ def twoDUniform(M,section):
           ax.set_xlabel("Number of Steps")
           plt.legend()
           plt.show()
-          
+
      if section == 3:
           x1 = [np.cumsum(r.choices([-1,1],k=M)) for i in range(5)]
           y1 = [np.cumsum(r.choices([-1,1],k=M)) for i in range(5)]
@@ -78,7 +91,6 @@ def twoDUniform(M,section):
           y = []
           for i in range(5):
                theta = np.cumsum(r.choices([0,360],k=M))
-               print(theta)
                x.append(5*np.cos(theta))
                y.append(5*np.sin(theta))
           fig = plt.figure(figsize=(10,10))
@@ -92,6 +104,24 @@ def twoDUniform(M,section):
           plt.legend()
           plt.show()
           
+     if section == 4:
+          x1 = np.array([s.mean([sum(r.choices([-1,1],k=M)) for i in range(int(np.sqrt(M)))]) for N in range(int(np.sqrt(M)))])
+          y1 = np.array([s.mean([sum(r.choices([-1,1],k=M)) for i in range(int(np.sqrt(M)))]) for N in range(int(np.sqrt(M)))])
+
+          x2_avecar = [np.average(np.square([sum(r.choices([-1,1],k=M)) for i in range(int(np.sqrt(M)))])) for N in range(5)]
+          y2_avecar = [np.average(np.square([sum(r.choices([-1,1],k=M)) for i in range(5)])) for N in range(5)]
+           
+          #ok
           
+          fig = plt.figure()
+          ax = fig.add_subplot(111, projection = '3d')
+          ax.plot(x1,y1,range(len(x1)))
+          ax.plot([0]*len(x1),[0]*len(x1),range(len(x1)))
+          plt.show()
           
      
+     
+     
+
+   
+       
